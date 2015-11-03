0808bea23f416197b998f310222a7dc2cfd0a093
Andrew Sun <ajsun@princeton.edu>
2015-11-03 16:00:37 -0500

    fixed bug for 1x1 and 2x2

--- vhessenberg.R
+++ vhessenberg.R
@@ -1,6 +1,6 @@
 vhessenberg <- function(x) {
   n = length(x)
-  x = x/norm(x,"2")
+  x = x/norm(x,"F")
   s = t(x[2:n]) %*% x[2:n]
   v = matrix(c(1, x[2:n]))
   if (s == 0) {
@@ -24,18 +24,24 @@ hess <- function(A) {
   n = max(dim(A))
   Q = diag(n)
   H = A
-  for (k in 1:(n-2)) {
-    B = vhessenberg(H[(k+1):n,k])
-    v = B$v
-    beta = B$beta
-    I = diag(k)
-    N = matrix(0, k, n-k)
-    m = length(v)
-    R = diag(m) - as.vector(beta)* v %*% t(v)
-    H[(k+1):n,k:n] = R %*% H[(k+1):n, k:n]
-    H[1:n, (k+1):n] = H[1:n, (k+1):n] %*% R
-    P = rbind(cbind(I,N),cbind(t(N), R))
-    Q = Q %*% P
+  if (n <= 2) {
+    H = A
+    Q = diag(n)
+  }
+  else {
+    for (k in 1:(n-2)) {
+      B = vhessenberg(as.matrix(H[(k+1):n,k]))
+      v = B$v
+      beta = B$beta
+      I = diag(k)
+      N = matrix(0, k, n-k)
+      m = length(v)
+      R = diag(m) - as.vector(beta)* v %*% t(v)
+      H[(k+1):n,k:n] = R %*% H[(k+1):n, k:n]
+      H[1:n, (k+1):n] = H[1:n, (k+1):n] %*% R
+      P = rbind(cbind(I,N),cbind(t(N), R))
+      Q = Q %*% P
+    }
   }
   return(list(H = H, Q = Q))
 }
